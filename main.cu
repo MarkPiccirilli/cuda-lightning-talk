bc1ceb97c46c88e3c88829a6c72b466325b49fb6
Piccirilli <mark.r.piccirilli@accenture.com>
2023-03-16 22:40:52 -0400

    updates

--- main.cu
+++ main.cu
@@ -7,20 +7,21 @@
 #define ARRAYSIZE 1000000
 #endif
 
-//#ifndef THREADARRAY
-//#define THREADARRAY (int[]){1, 2, 4, 6, 8, 12, 16}
-//#endif
-
 #ifndef BLOCKSIZE
 #define BLOCKSIZE 256
 #endif
 
 #ifndef NUMBLOCKS
-#define NUMBLOCKS 0
+#define NUMBLOCKS NULL
+#endif
+
+#ifndef NUMTRIES
+#define NUMTRIES 5
 #endif
 
 using std::cout;
 using std::cerr;
+using std:: endl;
 
 __global__
 void cudaMultiply(int *array1, int *array2, int *array3, int size) {
@@ -28,13 +29,6 @@ void cudaMultiply(int *array1, int *array2, int *array3, int size) {
     int stride = blockDim.x * gridDim.x;
     for (int i = index; i < size; i += stride)
         array3[i] = array1[i] * array2[i];
-}
-
-void printArray(int *array, int arraySize) {
-    for(int i = 0; i < arraySize; i++) {
-        cout << array[i] << " ";
-    }
-    cout << '\n';
 }
 
 int main(int argc, char **argv) {
@@ -59,16 +53,20 @@ int main(int argc, char **argv) {
 
     for(int i = 0; i < threadArraySize; i++) {
         omp_set_num_threads(threadArray[i]);
-        double timeStart = omp_get_wtime();
 
-        #pragma omp parallel for
-        for(int j = 0; j < arraySize; j++) {
-            hostArray3[j] = hostArray1[j] * hostArray2[j];
+        double totalTime = 0;
+        for(int j = 0; j < NUMTRIES; j++) {
+            double timeStart = omp_get_wtime();
+            #pragma omp parallel for
+            for(int k = 0; k < arraySize; k++) {
+                hostArray3[k] = hostArray1[k] * hostArray2[k];
+            }
+            double timeComplete = omp_get_wtime();
+            totalTime = totalTime + timeComplete - timeStart;
         }
+        double averageTime = totalTime / NUMTRIES;
 
-        double timeComplete = omp_get_wtime();
-        double totalTime = timeComplete - timeStart;
-        cout << "Time to complete operation with " << threadArray[i] << " threads(ms): " << totalTime * 1000 << '\n';
+        cout << "Time to complete operation with " << threadArray[i] << " threads(ms): " << averageTime * 1000 << '\n';
     }
 
     int *deviceArray1, *deviceArray2, *deviceArray3;
@@ -84,18 +82,27 @@ int main(int argc, char **argv) {
     cudaEventCreate(&stop);
 
     int blockSize = BLOCKSIZE;
-    int numBlocks = NUMBLOCKS ? (arraySize + blockSize - 1) / blockSize : NUMBLOCKS;
+    int numBlocks = NUMBLOCKS ? NUMBLOCKS : (arraySize + blockSize - 1) / blockSize;
 
-    cudaEventRecord(start, NULL);
-    cudaMultiply<<<numBlocks, blockSize>>>(deviceArray1, deviceArray2, deviceArray3, arraySize);
-    cudaEventRecord(stop, NULL);
-    cudaEventSynchronize(stop);
+    cout << "Block Size: " << blockSize << endl;
+    cout << "Number of Blocks: " << numBlocks << endl;
 
-//    cudaDeviceSynchronize();
+    double totalTimeGPU = 0.0;
+    for(int i = 0; i < NUMTRIES; i++) {
+        cudaEventRecord(start, NULL);
+        cudaMultiply<<<numBlocks, blockSize>>>(deviceArray1, deviceArray2, deviceArray3, arraySize);
+        cudaEventRecord(stop, NULL);
+        cudaEventSynchronize(stop);
 
-    float totalTimeGPU = 0.0f;
-    cudaEventElapsedTime(&totalTimeGPU, start, stop);
-    cout << "Time to complete operation with the GPU using blocksize: " << blockSize << " and numBlocks: " << numBlocks << " was(ms): " << totalTimeGPU << '\n';
+        float runTimeGPU = 0.0f;
+        cudaEventElapsedTime(&runTimeGPU, start, stop);
+
+        totalTimeGPU = totalTimeGPU + runTimeGPU;
+    }
+
+    double averageTimeGPU = totalTimeGPU / NUMTRIES;
+
+    cout << "Time to complete operation with the GPU using blocksize: " << blockSize << " and numBlocks: " << numBlocks << " was(ms): " << averageTimeGPU << '\n';
 
     // Free memory
     delete [] hostArray1;
